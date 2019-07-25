dc8fc4fe7fd50158647e19630005500506bcfde7
Avinash Dimmeta <avinash514@gmail.com>
2019-07-25 13:39:24 -0400

    Tested level1.sh Succesfuly

--- level1.sh
+++ level1.sh
@@ -13,48 +13,73 @@ sudo yum install -y kubectl gcloud google-cloud-sdk-app-engine-grpc google-cloud
 projectName=$1
 billing_account=`gcloud alpha billing accounts list | sed '1d' | awk '{print $1}'`
 
-if [ "" == "" ];
+if [ "$billing_account" == "" ];
 then
 echo "No Billing acount found in your config"
 echo "Please config gcloud in local terminal"
 exit 1
 fi
 
+get_status(){
+if [ $? -eq '0' ];
+then
+echo "SUCCESS"
+else
+echo "FAILED"
+exit 1
+fi
+}
+
+
 gcloud projects create $projectName --set-as-default
+get_status
 gcloud config set compute/zone "us-central1-a"
+get_status
 echo "gcp $projectName is created in zone us-central1-a"
 
 #CLuster Creation
 gcloud alpha billing projects link $projectName --billing-account $billing_account
-gcloud container clusters create "my-first-cluster-1" --zone "us-central1-a"
+get_status
+gcloud container clusters create "$1" --zone "us-central1-a" --enable-basic-auth
+get_status
 
 #To Setup kube config
-gcloud container clusters get-credentials "my-first-cluster-1"
+gcloud container clusters get-credentials "$1"
+get_status
 
 curl -o get_helm.sh https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get
 chmod +x get_helm.sh
 ./get_helm.sh
 
 kubectl create serviceaccount --namespace kube-system tiller
+get_status
 kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
+get_status
 helm init --service-account tiller --wait
+get_status
 helm install --name nginx-ingress stable/nginx-ingress --set rbac.create=true --set controller.publishService.enabled=true
+get_status
 kubectl create namespace staging
+get_status
 kubectl create namespace production
+get_status
 
 #Deploying application to Staging Namespace
 
 echo "########### Staging Service to Browse #############"
-kubectl config use-context staging
-kubectl apply -f guestbook-all-in-one.yaml
+kubectl config set-context --current --namespace=staging
+kubectl apply -f guest-book.yaml
+sleep 10 
 kubectl get svc frontend -n staging
 echo "###################################################"
 
 echo "########### Production Service to Browse #############"
-kubectl config use-context production
-kubectl apply -f guestbook-all-in-one.yaml 
+kubectl config set-context --current --namespace=production
+kubectl apply -f guest-book.yaml
+sleep 10 
 kubectl get svc frontend -n production
 echo "###################################################"
 
 # Deployment Autoscalling on CPU Percentage
+
 #kubectl autoscale deployment frontend --min=2 --max=10 --cpu-percent=80
